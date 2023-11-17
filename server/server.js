e51a9e9b5701cd3b91e456d1970d3ca8de25683f
Charlene Kay Bakker <c.k.bakker@student.tudelft.nl>
2023-11-17 21:15:05 +0100

    Made small change

--- server/server.js
+++ server/server.js
@@ -1,1 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require("./routes")
+const url = "mongodb://127.0.0.1/taskListDB"
 
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public")) //?
+
+app.use(bodyParser.urlencoded({
+extended: true
+}));
+
+try{
+    mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log('MongoDB connected')
+    })
+} catch (err) {
+    console.error(err.message)
+    process.exit(1)
+}
+
+app.get("/", (req, res) => {
+    res.send({ message: "Hello!"})
+})
+
+app.listen(3000, function() {
+        console.log("Server started on port 3000")
+});
+
+
+// Connect to MongoDB database
+//https://rahmanfadhil.com/express-rest-api/
+// mongoose.connect('mongodb://localhost:27017/taskList', { useNewUrlParser: true}) //what is unifiedTopology?
+//   .then(() => {
+
+//     //create Express server
+//     const app = express()
+
+//     app.use("/api", routes)
+
+//     app.listen(3000, function() {
+//         console.log("Server started on port 3000")
+//     });
+    
+//     console.log('MongoDB connected')
+// })
+// .catch((err) => console.log(err));
