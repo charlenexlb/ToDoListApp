329cacfdc535bc090821cc098c471fe5f11e860b
Charlene Kay Bakker <c.k.bakker@student.tudelft.nl>
2023-11-17 20:46:26 +0100

    Made small change

--- server/db.js
+++ server/db.js
@@ -1,8 +1,33 @@
-// Connect to MongoDB database
-mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true}) //what is unifiedTopology?
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log(err));
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require("./routes")
 
-app.listen(3000, function() {
-    console.log("Server started on port 3000")
-});
+// const app = express();
+
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+// app.use(express.static("public")) //?
+
+// app.use(bodyParser.urlencoded({
+// extended: true
+// }));
+
+// Connect to MongoDB database
+//https://rahmanfadhil.com/express-rest-api/
+mongoose.connect('mongodb://localhost:27017/taskList', { useNewUrlParser: true}) //what is unifiedTopology?
+  .then(() => {
+
+    //create Express server
+    const app = express()
+
+    app.use("/api", routes)
+
+    app.listen(3000, function() {
+        console.log("Server started on port 3000")
+    });
+    
+    console.log('MongoDB connected')
+})
+.catch((err) => console.log(err));
