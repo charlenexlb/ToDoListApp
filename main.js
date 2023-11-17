d2c907b7c53d3fa1833e0f95355a7dfd5e44870b
Charlene Kay Bakker <c.k.bakker@student.tudelft.nl>
2023-11-17 20:47:55 +0100

    Made small change

---
+++ main.js
@@ -0,0 +1,28 @@
+// Modules to control app life nd create native browser window
+const {app, BrowserWindow} = require('electron')
+const path = require('node:path')
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    mainWindow.loadFile('./client/index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+//listens to window-all-closed event and quits the app is user not on macOS
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
