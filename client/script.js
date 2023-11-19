14fd0611a6570d70a00c1663d48b2cc60343e3ca
Charlene Kay Bakker <c.k.bakker@student.tudelft.nl>
2023-11-19 21:15:48 +0100

    Implemented basic frontend functionality

--- client/script.js
+++
@@ -1,8 +0,0 @@
-const addBtn = document.getElementById("addBtn");
-addBtn.addEventListener('click', addNewTask());
-
-function addNewTask(){
-    var tasklist = document.getElementById("tasklist");
-    var li = document.createElement("li");
-    tasklist.appendChild(li);
-}
