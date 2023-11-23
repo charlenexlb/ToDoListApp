eb6315993839526fc785a5868cf357386d41bcac
Charlene Kay Bakker <c.k.bakker@student.tudelft.nl>
2023-11-23 10:41:15 +0100

    Removed a link

--- server/database.js
+++ server/database.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 const url = "mongodb://127.0.0.1/taskListDB"
 
-// https://blog.appsignal.com/2023/08/09/how-to-use-mongodb-and-mongoose-for-nodejs.html
 // Database connect function
 function connectDB(){
     try{
